1dda98fba1b6743547f57c770432acb4896532fc
Thái Truong <Truongquocthai627@gmail.com>
2026-03-04 13:42:03 +0700

    Add files via upload

--- src/DAO/DBConnection.java
+++ src/DAO/DBConnection.java
@@ -1,26 +1,29 @@
-package DAO;
+package Utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBConnection {
 
+    // 🔧 SỬA THÔNG TIN THEO DATABASE CỦA BẠN
     private static final String URL =
-        "jdbc:sqlserver://localhost:1433;"
-        + "databaseName=QuanLyCuaHangLaptop;"
-        + "encrypt=true;trustServerCertificate=true";
-
-    private static final String USER = "sa";   
-    private static final String PASSWORD = "1";  
+            "jdbc:sqlserver://localhost:1433;databaseName=;encrypt=true;trustServerCertificate=true";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() {
         try {
+            // Load driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
+    public static Connection getConn() {
+    return getConnection();
 }
+}
+
